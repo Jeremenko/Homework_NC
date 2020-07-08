ffed94f9139490893aae80d2f74cbac15c494a57
Jeremenko <komarik100@gmail.com>
2020-07-08 21:44:19 +0300

    update

--- src/main/java/ua/edu/sumdu/ta/practice2/Task.java
+++ src/main/java/ua/edu/sumdu/ta/practice2/Task.java
@@ -4,32 +4,77 @@ public class Task {
 
     private String title;
     private boolean active = false;
+    private int time ;
 
-    private int time;
-    private int repeat;
-    private int startTime;
-    private int endTime;
-    private int repeatInterval;
+    private int startTime = 0;
+    private int endTime = 0;
+    private int repeatInterval = 0;
 
-    private Task(String title, int time){
-        this.title=title;
-        this.time=time;
-    };
-    private Task(String title, int time, int repeat){
-        this.title=title;
-        this.time=time;
-        this.repeat=repeat;
-    };
+
+    private Task(String title, int time) {
+        this.title = title;
+        this.time = time;
+    }
+
+
+
+    private Task(String title, int time, int repeat) {
+        this.title = title;
+        this.time = time;
+        this.repeatInterval = repeat;
+    }
+
+
 
     public String getTitle() {
         return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public boolean isActive() {
+        return this.active;
+    }
+
+    public void setTime(int time) {
+        if (time >= 0) this.time = time;
+        else System.out.println("Введите корректное время");
     }
 
     public int getTime() {
         return time;
     }
 
-    public int getRepeat() {
-        return repeat;
+
+    public void setTime(int start, int end, int repeat) {
+        if (start >= 0) this.startTime = start;
+        else System.out.println("Введите корректное время старта");
+        if (end >= start) this.endTime = end;
+        else System.out.println("Введите корректное время окончания");
+        if (repeat > 0) this.repeatInterval = repeat;
+        else System.out.println("Введите корректное число повторений");
     }
+
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public int getRepeatInterval() {
+        if (repeatInterval > 0)
+            return repeatInterval;
+        else
+            return 0;
+    }
+
 }
